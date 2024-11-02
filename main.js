0055b78a72e612c06a563b6a6b07d80b7879375f
Miza <mizaeron@gmail.com>
2024-11-02 19:58:54 +0000

    Create method for adding X to the board

---
+++ main.js
@@ -0,0 +1,16 @@
+const gameBoard = {
+    board: [],
+
+};
+
+const players = {
+
+};
+
+const gameFlow = {
+    firstInput: function(item) {
+        gameBoard.board.push(item);
+    }
+};
+
+gameFlow.firstInput("X");
