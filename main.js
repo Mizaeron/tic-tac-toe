d85ba66f4448be0939d7b3903082146e48d40533
Miza <mizaeron@gmail.com>
2024-11-04 19:14:33 +0000

    Create winning conditions check
    add gameboard combitation arrays that determine the winner
    write a function that destructures the arrays into indices
    indices check if they are the same and return winner

--- main.js
+++ main.js
@@ -1,16 +1,35 @@
+
 const gameBoard = {
-    board: [],
+    board: ['', '', 'X', '', 'X', 'X', '', '', ''],
+    winConditions: [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]
+    ],
+    winnerCheck: function() {
+        for (const condition of this.winConditions) {
+            const [a, b, c] = condition;
+
+            if (this.board[a] && this.board[a] === this.board[b] && this.board[a] === this.board[c]) {
+                console.log(`Winner: ${this.board[a]}`);
+                return this.board[a];
+            }
+        }
+        console.log('No winner');
+        return null;
+    }
 
 };
+
+gameBoard.winnerCheck();
 
 const players = {
 
 };
 
 const gameFlow = {
-    firstInput: function(item) {
-        gameBoard.board.push(item);
+    firstInput: function(item, pos) {
+        gameBoard.board.push(item, pos);
     }
 };
 
-gameFlow.firstInput("X");
